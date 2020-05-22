4fe08709f521c195a47a332c02497762eb31a292
laurayesayan <laurayesayan@gmail.com>
2020-05-22 17:32:39 +0300

    Initial Commit

---
+++ UnitTestsWithQuick_2_18/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitTestsWithQuick_2_18
+//
+//  Created by Лаура Есаян on 22.05.2020.
+//  Copyright © 2020 LY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UnitTestsWithQuick_2_18Tests/UnitTestsWithQuick_2_18Tests.swift
@@ -0,0 +1,34 @@
+//
+//  UnitTestsWithQuick_2_18Tests.swift
+//  UnitTestsWithQuick_2_18Tests
+//
+//  Created by Лаура Есаян on 22.05.2020.
+//  Copyright © 2020 LY. All rights reserved.
+//
+
+import XCTest
+@testable import UnitTestsWithQuick_2_18
+
+class UnitTestsWithQuick_2_18Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
